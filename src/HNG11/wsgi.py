38b63c648f88ec38bb1fece9f4e9aaf5f3b66b47
Akere Aishat <mojiradeaisha850@gmail.com>
2024-07-02 21:40:11 +0100

    update

--- src/HNG11/wsgi.py
+++ src/HNG11/wsgi.py
@@ -16,3 +16,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HNG11.settings')
 application = get_wsgi_application()
 
 app=application
+
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
