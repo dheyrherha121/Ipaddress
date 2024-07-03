d6b2059e8696d9ff6a54a836f02a9380aaa043f4
Akere Aishat <mojiradeaisha850@gmail.com>
2024-07-03 22:06:02 +0100

    update

--- src/tesk1/views.py
+++ src/tesk1/views.py
@@ -8,10 +8,10 @@ def hello(request):
    
     visitor_name= request.GET.get('visitor_name')
     
-    client_ip=request.META.get('REMOTE_ADDR')
-    token= '1b6a79055dd6a8'
-    ip_url=get(f"https://ipinfo.io/{client_ip}/city/json?token={token}").json()
-    #ip=ip_url.get("ip")
+    #client_ip=request.META.get('REMOTE_ADDR')
+    apikey='c6fa6d83950f4c05b22a0ef4526b5eff' 
+    ip_url=get(f"https://api.ipgeolocation.io/ipgeo?apiKey={apikey}").json()
+    ip=ip_url.get("ip")
     city=ip_url.get("city")
 
     api_key = "43155ad1b40cf402395c590aab20fac1"
@@ -20,7 +20,7 @@ def hello(request):
     response=get(complete_url).json()
     temp=response['main']['temp']
 
-    response= {'client_ip':client_ip,'location':city, 'greeting':f'hello {visitor_name}!,the temperature is {temp} degree Celsius in {city} '}
+    response= {'client_ip':ip,'location':city, 'greeting':f'hello {visitor_name}!,the temperature is {temp} degree Celsius in {city} '}
     return JsonResponse(response)
 
 
